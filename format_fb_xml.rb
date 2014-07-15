61c2ef05dafa68bd2270a600da76f24ba6732b2e
António Gorgulho <antoniosgorgulho@gmail.com>
2014-07-15 12:17:46 +0100

    minor fix

--- format_fb_xml.rb
+++ format_fb_xml.rb
@@ -164,28 +164,22 @@ def push_file_to_fb(browser, file)
 	end
 
 	translations.each_pair do |k, v|
-		browser.goto("https://www.facebook.com/translations/admin/?app=742586302475822&query=#{k}&loc=#{locale}")
-
 		if v.strip.length > 0
-			if (!browser.div(class: 'clearfix voting_row').exists?)
-				browser.textarea(name: "translation").click
-				#browser.textarea(name: "translation").set(v)
-				
-				browser.wait_until(5) { browser.div(class: 'trans_bar_actions').button(name: "submit").exists? }
-				
-				# while browser.textarea(name: 'translation').text != v
-				# 	browser.textarea(name: "translation").set(v)
-				# end
+			browser.goto("https://www.facebook.com/translations/admin/?app=742586302475822&query=#{k}&loc=#{locale}")
+				if (!browser.div(class: 'clearfix voting_row').exists?)
+					browser.textarea(name: "translation").click
+					browser.textarea(name: "translation").set(v.strip)
+					
+					browser.wait_until(5) { browser.div(class: 'trans_bar_actions').button(name: "submit").exists? }
+					
+					begin
+						browser.div(class: 'trans_bar_actions').button(text: "Translate").click
 
-				begin
-					browser.div(class: 'trans_bar_actions').button(text: "Translate").click
-
-					sleep(1)
-
-					if browser.link(class: 'layerCancel').exists?
-						browser.textarea(name: "translation").set(v)
+						if browser.link(class: 'layerCancel').exists?
+							browser.textarea(name: "translation").set(v.strip)
+						end
+					rescue
 					end
-				rescue
 				end
 			end
 		end
